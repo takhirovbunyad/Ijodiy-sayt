1f6a4236d57dec77bb9e16bd327b58c9eb9d9a18
takhirovbunyad <takhirovbunyad@gmail.com>
2025-07-30 11:41:59 +0500

    adding supbase postgres db

--- config/settings.py
+++ config/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '#_F/83U%Frz/mYJ',
+        'HOST': 'db.bawbhkubtykqquappris.supabase.co',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
